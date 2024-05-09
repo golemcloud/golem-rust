676ea32107fc6daef183c5b35e22a57eb2bc28c4
sviezypan <jaroslav.regec1@gmail.com>
2024-05-09 13:02:29 -0600

    cleanup

--- golem-rust-examples/src/main.rs
+++ golem-rust-examples/src/main.rs
@@ -27,20 +27,60 @@ enum Color {
     BLUE,
 }
 
-// type Addy = string;
-struct Addy(String);
 
-// #[golem()]
-// fn get_address() -> Address {
-//     Address {
-//         street: "".to_owned(),
-//         city: "".to_owned(),
-//         state: "".to_owned(),
-//         zip: "".to_owned(),
-//     }
+
+pub enum IpAddrEmpty {
+    V4,
+    V6,
+}
+
+pub struct BidderId {
+    pub bidder_id: std::result::Result<IpAddrEmpty, String>,
+    pub verified: bool,
+}
+
+// impl HasWitMetadata for IpAddrEmpty {
+//     const IDENT: &'static str = "IpAddrEmpty";
+
+//     const WIT: &'static WitMeta = &WitMeta::Enum(EnumMeta {
+//         name: Ident("IpAddrEmpty"),
+//         variants: &[ Ident("V4"), Ident("V6")],
+//     });
 // }
 
-//struct GetAddress {}
+// impl HasWitMetadata for BidderId {
+//     const IDENT: &'static str = "BidderId";
+
+//     const WIT: &'static WitMeta = &WitMeta::Record(RecordMeta {
+//         name: Ident("BidderId"),
+//         fields: &[
+//             ("bidder_id", Result::<IpAddrEmpty, String>::WIT),
+//             ("verified", bool::WIT),
+//         ]
+//     });
+// }
+
+// struct CreateBidder {}
+
+// impl HasWitMetadata for CreateBidder {
+//     const IDENT: &'static str = "create_bidder";
+
+//     const WIT: &'static WitMeta = &WitMeta::Function(FunctionMeta {
+//         name: Ident("create_bidder"),
+//         args: &[],
+//         result: &WitMeta::Tuple(&[BidderId::WIT, f32::WIT]),
+//     });
+// }
+
+//#[golem()]
+fn create_bidder(full_name: String, address: &[String], age: Option<u16>) -> (BidderId, f32) {
+    (BidderId {
+        bidder_id: Result::Err("hello".to_owned()),
+        verified: false
+    }, 35.6)
+}
+
+
 
 //#[golem()]
 fn get_address() -> Address {
@@ -53,66 +93,7 @@ fn get_address() -> Address {
     }
 }
 
-enum IpAddrEmpty {
-    V4,
-    V6,
-}
-
-impl HasWitMetadata for IpAddrEmpty {
-    const IDENT: &'static str = "IpAddrEmpty";
-
-    const WIT: &'static WitMeta = &WitMeta::Enum(EnumMeta {
-        name: Ident("IpAddrEmpty"),
-        variants: &[ Ident("V4"), Ident("V6")],
-    });
-}
-
-pub struct BidderId {
-    pub bidder_id: std::result::Result<IpAddrEmpty, String>,
-    pub verified: bool,
-}
-
-// impl <T, E> HasWitMetadata for Result<T,E>
-//     where
-//         T: HasWitMetadata,
-//         E: HasWitMetadata {
-//     const IDENT: &'static str = "Result<T,E>";
-
-//     const WIT: &'static WitMeta = &WitMeta::Empty;
-
-// }
-
-impl HasWitMetadata for BidderId {
-    const IDENT: &'static str = "BidderId";
-
-    const WIT: &'static WitMeta = &WitMeta::Record(RecordMeta {
-        name: Ident("BidderId"),
-        fields: &[
-            ("bidder_id", Result::<IpAddrEmpty, String>::WIT),
-            ("verified", bool::WIT),
-        ]
-    });
-}
-
-//#[golem()]
-fn create_bidder(full_name: String, address: &[String], age: Option<u16>) -> (BidderId, f32) {
-    (BidderId {
-        bidder_id: Result::Err("hello".to_owned()),
-        verified: false
-    }, 35.6)
-}
-
-struct CreateBidder {}
-
-// impl HasWitMetadata for CreateBidder {
-//     const IDENT: &'static str = "create_bidder";
-
-//     const WIT: &'static WitMeta = &WitMeta::Function(FunctionMeta {
-//         name: Ident("create_bidder"),
-//         args: &[],
-//         result: &WitMeta::Tuple(&[BidderId::WIT, f32::WIT]),
-//     });
-// }
+// struct GetAddress {}
 
 // impl HasWitMetadata for GetAddress {
 //     const IDENT: &'static str = "get_address";
@@ -123,38 +104,6 @@ struct CreateBidder {}
 //         result: Address::WIT,
 //     });
 // }
-
-// impl HasWitMetadata for Address {
-//     const IDENT: &'static str = "Address";
-
-//     const WIT: &'static WitMeta = &WitMeta::Struct(StructMeta {
-//         name: Ident("Address"),
-//         fields: &[
-//             ("street", String::WIT),
-//             ("city", String::WIT),
-//             ("state", String::WIT),
-//             ("zip", String::WIT),
-//         ],
-//     });
-// }
-
-// impl HasWitMetadata for Person {
-//     const IDENT: &'static str = "Person";
-
-//     const WIT: &'static WitMeta = &WitMeta::Struct(StructMeta {
-//         name: Ident("Person"),
-//         fields: &[("name", String::WIT), ("address", Address::WIT)],
-//     });
-// }
-
-// #[distributed_slice(ALL_WIT_TYPES_FOR_GOLEM)]
-// static PERSON_WIT: fn() -> &'static WitMeta = || Person::WIT;
-
-// #[distributed_slice(ALL_WIT_TYPES_FOR_GOLEM)]
-// static ADDRESS_WIT: fn() -> &'static WitMeta = || Address::WIT;
-
-// #[distributed_slice(ALL_WIT_TYPES_FOR_GOLEM)]
-// static FUN_WIT: fn() -> &'static WitMeta = || GetAddress::WIT;
 
 #[test]
 fn test_iter() {
